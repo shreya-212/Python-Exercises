1aea39fbb4132e4ba385eccb3530c3522a2b2568
shreya-212 <shreyaa.srs@gmail.com>
2025-10-12 20:10:53 +0530

    Example 4

---
+++ Basics/Simple_conditions_1/Ex4.py
@@ -0,0 +1,6 @@
+#Check if a number is divisible by both 3 and 5
+n=int(input("Enter a number: "))
+if (n%3==0 and n%5==0):
+    print("Divisible by both 3 and 5")
+else:
+    print("Not divisible by both 3 and 5")
