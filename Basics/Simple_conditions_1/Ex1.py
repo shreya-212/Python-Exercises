659af772ef2a2fd705d9b58a0ca837a2dea9c0a3
shreya-212 <shreyaa.srs@gmail.com>
2025-10-12 20:08:17 +0530

    Example 1

---
+++ Basics/Simple_conditions_1/Ex1.py
@@ -0,0 +1,10 @@
+#Take a number and print whether it's positive,negative or zero
+
+n=int(input("Enter a  number: "))
+if n<0:
+    print("Number is negative")
+elif n>0:
+    print("Number is positive")
+else:
+    print("Zero")
+
